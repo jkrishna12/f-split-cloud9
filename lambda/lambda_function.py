589e7bfb77cb1b118b6cd66b683cf7512d3383f2
jkrishna12 <152908825+jkrishna12@users.noreply.github.com>
2024-10-11 16:29:19 +0100

    Update lambda_function.py
    Added conditional statement to only perform chunking logic if object size greater than 1.5 max_threshold

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -6,11 +6,12 @@ import math
 from datetime import datetime
 import pandas as pd
 import re
-import sys
 
 def lambda_handler(event, context):
 
     max_file_size = 52428800
+    
+    threshold = 1.5 * max_file_size
     
     write_bucket = 'imdb-load-split-kc'
 
@@ -30,43 +31,45 @@ def lambda_handler(event, context):
     
     print(f'Object body {obj_body}, object size {obj_size}')
     
-    line_count = line_counter(obj_body)
+    # creates a boto3 s3 client resource that will be used to put objects
+    write_client = boto3.client('s3')
     
-    print(f'Line count of file is {line_count}')
+    # gets today's year, month, day
+    year, month, day = get_date()
     
-    chunksize = chunkisze_set(line_count, obj_size, max_file_size)
-    
-    print(f'Chunksize = {chunksize}')
-    
-    object_body_only = get_obj_body(bucket_name, key_name)
-    
-    # iterates over the object body based on the chunk size
-    for index, chunk in enumerate(pd.read_csv(object_body_only, chunksize = chunksize, delimiter = '\t')):
+    if obj_size <= threshold:
         
-        # creates a boto3 s3 client resource that will be used to put objects
-        write_client = boto3.client('s3')
+        df = pd.read_csv(obj_body, delimiter = '\t')
         
-        # gets today's year, month, day
-        year, month, day = get_date()
-        
-        # string containing the key name of the chunk
-        print(f"key name raw {key_name}")
-        key_name_write = key_name_generator(key_name, index, year, month, day)
-        
+        key_name_write = key_name_generator(key_name, year, month, day, index = None)
         print(f"key name {key_name_write}")
         
-        print("/////////////")
-        
         # writes the chunk to the specified s3 bucket
-        response = write_client.put_object(Body = dataframe_to_bytes(chunk), Bucket = write_bucket, Key = key_name_write)
+        response = write_client.put_object(Body = dataframe_to_bytes(df), Bucket = write_bucket, Key = key_name_write)
         
-        # print(response)
+    else:
+        line_count = line_counter(obj_body)
         
-        # if index == 2:
-        #     break
+        print(f'Line count of file is {line_count}')
         
-    print('/////////')
-    
+        chunksize = chunkisze_set(line_count, obj_size, max_file_size)
+        
+        print(f'Chunksize = {chunksize}')
+        
+        object_body_only = get_obj_body(bucket_name, key_name)
+        
+        # iterates over the object body based on the chunk size
+        for index, chunk in enumerate(pd.read_csv(object_body_only, chunksize = chunksize, delimiter = '\t')):
+            
+            key_name_write = key_name_generator(key_name, year, month, day, index = index)
+            print(f"key name {key_name_write}")
+            
+            # writes the chunk to the specified s3 bucket
+            response = write_client.put_object(Body = dataframe_to_bytes(chunk), Bucket = write_bucket, Key = key_name_write)
+            
+            if index == 2:
+                break
+        
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
@@ -92,6 +95,7 @@ def get_obj(bucket_name, key_name):
     
 def get_obj_size_loc(object_dict):
     """
+    
     """
     
     obj_body = object_dict['Body']
@@ -99,9 +103,12 @@ def get_obj_size_loc(object_dict):
     obj_size = object_dict['ContentLength']
     
     return obj_body, obj_size
-
-
+    
 def get_obj_body(bucket_name, key_name):
+    """
+    Returns object body of the object, necessary after using line_counter function
+    Pointer is reintialised to the beginning of object
+    """
     
     s3 = boto3.client('s3')
     
@@ -110,6 +117,60 @@ def get_obj_body(bucket_name, key_name):
     obj_body = obj['Body']
     
     return obj_body
+    
+def get_date():
+    """
+    Returns the relevant parts of the date
+    
+    Paramters:
+        None
+        
+    Returns:
+        year: string, year of today's date
+        month: string, month of today's date
+        day: string, day of today's date
+    """
+    
+    today_date_year = datetime.today().strftime('%Y')
+    
+    today_date_month = datetime.today().strftime('%m')
+    
+    today_date_day = datetime.today().strftime('%d')
+
+    return today_date_year, today_date_month, today_date_day
+    
+def dataframe_to_bytes(df):
+    
+    buffer = io.BytesIO()
+    
+    df.to_csv(buffer, index=False)
+    
+    return buffer.getvalue()
+    
+def key_name_generator(key_name_raw, year, month, day, index = None):
+    """
+    Function provides a string representing the key name of where the specific
+    chunk will be written to
+    Parameters:
+        folder_name: string, name of the folder to be written to, this case split_files
+        key_name_raw: string, raw name of the key_name
+        index: integer, number representing the chunk of the file 
+        year: string, current year
+        month: string, current month
+        day: string, current day
+    Returns:
+        key_name_write: string, location of where the file will be written to
+    """
+    
+    file_name = re.match(r"^(.*?)\.tsv", key_name_raw).group(1)
+    
+    if index == None:
+        key_name_write = file_name+'/'+year+'/'+month+'/'+day+'/'+file_name+'.tsv'
+        
+    else:
+        key_name_write = file_name+'/'+year+'/'+month+'/'+day+'/'+file_name+'_'+str(index)+'.tsv'
+    
+    return key_name_write
     
 def line_counter(obj_body):
     """
@@ -131,13 +192,6 @@ def line_counter(obj_body):
             
             # increment line_count by 1 each time
             line_count += 1
-            
-            # line_size = sys.getsizeof(line)
-            
-            # print(f'Line {line_count} size {line_size}')
-            
-            # if line_count == 10:
-            #     break
     
     return line_count
     
@@ -169,53 +223,3 @@ def chunkisze_set(file_lines, file_bytes, default_file_bytes):
         chunksize = file_lines
 
     return chunksize
-    
-def get_date():
-    """
-    Returns the relevant parts of the date
-    
-    Paramters:
-        None
-        
-    Returns:
-        year: string, year of today's date
-        month: string, month of today's date
-        day: string, day of today's date
-    """
-    
-    today_date_year = datetime.today().strftime('%Y')
-    
-    today_date_month = datetime.today().strftime('%m')
-    
-    today_date_day = datetime.today().strftime('%d')
-
-    return today_date_year, today_date_month, today_date_day
-    
-def dataframe_to_bytes(df):
-    
-    buffer = io.BytesIO()
-    
-    df.to_csv(buffer, index=False)
-    
-    return buffer.getvalue()
-    
-def key_name_generator(key_name_raw, index, year, month, day):
-    """
-    Function provides a string representing the key name of where the specific
-    chunk will be written to
-    Parameters:
-        folder_name: string, name of the folder to be written to, this case split_files
-        key_name_raw: string, raw name of the key_name
-        index: integer, number representing the chunk of the file 
-        year: string, current year
-        month: string, current month
-        day: string, current day
-    Returns:
-        key_name_write: string, location of where the file will be written to
-    """
-    
-    file_name = re.match(r"^(.*?)\.tsv", key_name_raw).group(1)
-    
-    key_name_write = file_name+'/'+year+'/'+month+'/'+day+'/'+file_name+'_'+str(index)+'.tsv'
-    
-    return key_name_write
